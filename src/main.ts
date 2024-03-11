e23c68a58616a13299f0e57d81d1a5281f2a5a9a
pepe <joseexposito@vortice.dev>
2024-03-11 18:24:01 +0100

    Creando LoginView.vue

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Button from "primevue/button"
+import InputText  from "primevue/inputtext";
+import Checkbox from "primevue/checkbox";
+import "primeflex/primeflex.css";
+import 'primevue/resources/themes/aura-light-green/theme.css'
+import 'primeicons/primeicons.css'
 
-createApp(App).mount('#app')
+const app  = createApp(App)
+
+app.use(PrimeVue)
+app.component('Button', Button)
+app.component('InputText', InputText)
+app.component('Checkbox', Checkbox)
+app.mount('#app')
+
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,28 +2,28 @@
 
     <div class="surface-card p-4 shadow-2 border-round w-full lg:w-6">
         <div class="text-center mb-5">
-            <img src="/images/blocks/logos/hyper.svg" alt="Image" height="50" class="mb-3" />
-            <div class="text-900 text-3xl font-medium mb-3">Welcome Back</div>
-            <span class="text-600 font-medium line-height-3">Don't have an account?</span>
-            <a class="font-medium no-underline ml-2 text-blue-500 cursor-pointer">Create today!</a>
+            <img src="/images/logos/hyper.svg" alt="Image" height="50" class="mb-3" />
+            <div class="text-900 text-3xl font-medium mb-3">Bienvenido</div>
+            <span class="text-600 font-medium line-height-3">¿No tienes cuenta?</span>
+            <a class="font-medium no-underline ml-2 text-blue-500 cursor-pointer">Crear cuenta</a>
         </div>
 
         <div>
             <label for="email1" class="block text-900 font-medium mb-2">Email</label>
-            <InputText id="email1" type="text" placeholder="Email address" class="w-full mb-3" />
+            <InputText id="email1" type="text" placeholder="Correo electrónico" class="w-full mb-3" />
 
-            <label for="password1" class="block text-900 font-medium mb-2">Password</label>
-            <InputText id="password1" type="password" placeholder="Password" class="w-full mb-3" />
+            <label for="password1" class="block text-900 font-medium mb-2">Contraseña</label>
+            <InputText id="password1" type="password" placeholder="Contraseña" class="w-full mb-3" />
 
             <div class="flex align-items-center justify-content-between mb-6">
                 <div class="flex align-items-center">
                     <Checkbox id="rememberme1" :binary="true" v-model="checked1" class="mr-2"></Checkbox>
                     <label for="rememberme1">Remember me</label>
                 </div>
-                <a class="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">Forgot password?</a>
+                <a class="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">He olvidado mi contraseña</a>
             </div>
 
-            <Button label="Sign In" icon="pi pi-user" class="w-full"></Button>
+            <Button label="Iniciar sesión" icon="pi pi-user" class="w-full"></Button>
         </div>
     </div>
 
@@ -35,6 +35,6 @@ const checked1 = true
 
 </script>
 
-<style scope>
+<style scoped>
 
 </style>
