dc453d65d0a7e81725caa75b99dff3ff6b94a9fa
pepe <joseexposito@vortice.dev>
2024-03-12 17:44:38 +0100

    Modificando componente LoginView.vue

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,18 +1,20 @@
 import { defineStore } from 'pinia'
 
+const API_ROOT: string = 'http://127.0.0.1:8000/api';
+
 export const useAuthStore = defineStore({
     id: 'auth',
     state: () => ({
         token: null,
         error: null,
-        isLoading: false
+        isLoading: false,
     }),
     actions:{
         async login(email:string, password:string): Promise<void>
         {
             try{
                 this.isLoading = true
-                const response = await fetch('http://127.0.0.1:8000/api/login_check', {
+                const response = await fetch(`${API_ROOT}/login_check`, {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
@@ -26,7 +28,7 @@ export const useAuthStore = defineStore({
 
                 const data = await response.json();
                 this.token = data.token
-                this.error = null
+                this.error = data.error
 
             }catch(error: any){
                 this.error = error.message
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,30 +1,35 @@
 <template>
+    <div class="align-items-center flex justify-content-center lg:px-8 md:px-6 px-4 py-8 surface-ground ng-star-inserted">
 
-    <div class="surface-card p-4 shadow-2 border-round w-full lg:w-6">
-        <div class="text-center mb-5">
-            <img src="/images/logos/hyper.svg" alt="Image" height="50" class="mb-3" />
-            <div class="text-900 text-3xl font-medium mb-3">Bienvenido</div>
-            <span class="text-600 font-medium line-height-3">¿No tienes cuenta?</span>
-            <a class="font-medium no-underline ml-2 text-blue-500 cursor-pointer">Crear cuenta</a>
-        </div>
-
-        <div>
-            <label for="email1" class="block text-900 font-medium mb-2">Email</label>
-            <InputText v-model="email" id="email1" type="text" placeholder="Correo electrónico" class="w-full mb-3" />
-
-            <label for="password1" class="block text-900 font-medium mb-2">Contraseña</label>
-            <InputText v-model='password' id="password1" type="password" placeholder="Contraseña" class="w-full mb-3" />
-
-            <div class="flex align-items-center justify-content-between mb-6">
-                <div class="flex align-items-center">
-                    <Checkbox id="rememberme1" :binary="true" v-model="checked1" class="mr-2"></Checkbox>
-                    <label for="rememberme1">Remember me</label>
-                </div>
-                <a class="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">He olvidado mi contraseña</a>
+        <div class="surface-card p-4 shadow-2 border-round w-full lg:w-6">
+            <div class="text-center mb-5">
+                <img src="../assets/images/logos/hyper.svg" alt="Image" height="50" class="mb-3" />
+                <div class="text-900 text-3xl font-medium mb-3">Bienvenido</div>
+                <span class="text-600 font-medium line-height-3">¿No tienes cuenta?</span>
+                <a class="font-medium no-underline ml-2 text-blue-500 cursor-pointer">Crear cuenta</a>
             </div>
 
-            <Button @click="login" label="Iniciar sesión" icon="pi pi-user" class="w-full"></Button>
+            <div>
+
+                <label for="email1" class="block text-900 font-medium mb-2">Email</label>
+                <InputText v-model="email" id="email1" type="text" placeholder="Correo electrónico" class="w-full mb-3"
+                           :invalid="store.error !== null"/>
+
+                <label for="password1" class="block text-900 font-medium mb-2">Contraseña</label>
+                <InputText v-model='password' id="password1" type="password" placeholder="Contraseña" class="w-full mb-3" />
+
+                <div class="flex align-items-center justify-content-between mb-6">
+                    <div class="flex align-items-center">
+                        <Checkbox id="rememberme1" :binary="true" v-model="checked1" class="mr-2"></Checkbox>
+                        <label for="rememberme1">Remember me</label>
+                    </div>
+                    <a class="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">He olvidado mi contraseña</a>
+                </div>
+
+                <Button @click="login" label="Iniciar sesión" icon="pi pi-user" class="w-full"></Button>
+            </div>
         </div>
+
     </div>
 
 </template>
